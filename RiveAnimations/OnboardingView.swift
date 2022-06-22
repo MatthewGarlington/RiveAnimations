2f665f46e3bb0622a24365a9e590b1c4ee53bccf
Matthew Garlington <61671107+MatthewGarlington@users.noreply.github.com>
2022-06-21 23:06:25 -0400

    feat: Initial commit adding assets and OnboardingView

---
+++ RiveAnimations/OnboardingView.swift
@@ -0,0 +1,28 @@
+//
+//  OnboardingView.swift
+//  RiveAnimations
+//
+//  Created by Matthew Garlington on 6/21/22.
+//
+
+import SwiftUI
+import RiveRuntime
+
+struct OnboardingView: View {
+    var body: some View {
+        RiveViewModel(fileName: "shapes").view()
+            .ignoresSafeArea()
+            .blur(radius: 30)
+            .background(
+                Image("Spline")
+                    .blur(radius: 50)
+                    .offset(x: 200, y: 100)
+            )
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
