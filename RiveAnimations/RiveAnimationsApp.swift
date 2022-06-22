e17d2278c6d1d08521c344a5a398325cea308345
Matthew Garlington <61671107+MatthewGarlington@users.noreply.github.com>
2022-06-21 22:58:53 -0400

    Initial Commit

---
+++ RiveAnimations/RiveAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiveAnimationsApp.swift
+//  RiveAnimations
+//
+//  Created by Matthew Garlington on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct RiveAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
